0448b39d8596bedc93553f3d3d01d78898735d50
mmhobi7 <woohoomoo2u@gmail.com>
2024-04-03 22:04:01 -0400

    gen shader once

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <hyprland/src/helpers/Box.hpp>
 
 #include <hyprland/src/render/Framebuffer.hpp>
+#include <hyprland/src/render/shaders/Textures.hpp>
 #include <string>
 #include <unistd.h>
 
@@ -19,19 +20,6 @@ inline HANDLE PHANDLE = nullptr;
 
 // Do NOT change this function.
 APICALL EXPORT std::string PLUGIN_API_VERSION() { return HYPRLAND_API_VERSION; }
-
-class CWindowTransformer : public IWindowTransformer {
-public:
-  CWindowTransformer() {}
-
-  virtual CFramebuffer *transform(CFramebuffer *in);
-  virtual void preWindowRender(SRenderData *pRenderData, CFramebuffer *in,
-                               struct wlr_surface *surface);
-
-private:
-  unsigned int shader, vertex, fragment;
-};
-std::vector<CWindowTransformer *> ptrs;
 
 // Shader sources
 const GLchar *vertexSource =
@@ -74,42 +62,103 @@ const GLchar *fragmentSource =
 // glDeleteShader(vertCompiled);
 // glDeleteShader(fragCompiled);
 
+class CWindowTransformer : public IWindowTransformer {
+public:
+  CWindowTransformer() {
+    // vertex = glCreateShader(GL_VERTEX_SHADER);
+    // // glShaderSource(shader, 1, (const GLchar **)&shaderSource, nullptr);
+    // glShaderSource(vertex, 1, (const GLchar **)&vertexSource, NULL);
+    // glCompileShader(vertex);
+    // // checkCompileErrors(vertex, "VERTEX");
+    // // fragment Shader
+    // fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    // glShaderSource(fragment, 1, (const GLchar **)&fragmentSource, NULL);
+    // glCompileShader(fragment);
+    // // checkCompileErrors(fragment, "FRAGMENT");
+    // // shader Program
+    // prog = glCreateProgram();
+    // glAttachShader(prog, vertex);
+    // glAttachShader(prog, fragment);
+    // glLinkProgram(prog);
+    // // checkCompileErrors(ID, "PROGRAM");
+    // // delete the shaders as they're linked into our program now and no
+    // // longer
+    // // necessary
+    // glDeleteShader(vertex);
+    // glDeleteShader(fragment);
+    // shader.program = prog;
+    // shader.proj = glGetUniformLocation(prog, "proj");
+    // shader.tex = glGetUniformLocation(prog, "tex");
+    // shader.texAttrib = glGetAttribLocation(prog, "texcoord");
+    // shader.posAttrib = glGetAttribLocation(prog, "pos");
+  }
+
+  virtual CFramebuffer *transform(CFramebuffer *in);
+  virtual void preWindowRender(SRenderData *pRenderData, CFramebuffer *in,
+                               struct wlr_surface *surface);
+
+private:
+  // CShader shader;
+  CFramebuffer fb;
+  // unsigned int prog, vertex, fragment;
+};
+std::vector<CWindowTransformer *> ptrs;
+
+inline const std::string myTEXFRAGSRCRGBAPASSTHRU = R"#(
+precision highp float;
+varying vec2 v_texcoord; // is in 0-1
+uniform sampler2D tex;
+
+void main() {
+    gl_FragColor = texture2D(tex, v_texcoord);
+})#";
+
 void CWindowTransformer::preWindowRender(SRenderData *pRenderData,
                                          CFramebuffer *in,
                                          struct wlr_surface *surface) {
 
   // oldPos = {(double)pRenderData->x, (double)pRenderData->y};
-  CFramebuffer fb;
+  // CFramebuffer fb;
 
   if (!fb.isAllocated() || fb.m_vSize != in->m_vSize) {
     fb.release();
     fb.alloc(in->m_vSize.x, in->m_vSize.y);
   }
+  fb.bind();
 
   const auto TEXTURE = wlr_surface_get_texture(pRenderData->surface);
   const CTexture &tex = TEXTURE;
-
   // 2. compile shaders
-  vertex = glCreateShader(GL_VERTEX_SHADER);
-  // glShaderSource(shader, 1, (const GLchar **)&shaderSource, nullptr);
-  glShaderSource(vertex, 1, (const GLchar **)&vertexSource, NULL);
-  glCompileShader(vertex);
-  // checkCompileErrors(vertex, "VERTEX");
-  // fragment Shader
-  fragment = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragment, 1, (const GLchar **)&fragmentSource, NULL);
-  glCompileShader(fragment);
-  // checkCompileErrors(fragment, "FRAGMENT");
-  // shader Program
-  shader = glCreateProgram();
-  glAttachShader(shader, vertex);
-  glAttachShader(shader, fragment);
-  glLinkProgram(shader);
-  // checkCompileErrors(ID, "PROGRAM");
-  // delete the shaders as they're linked into our program now and no longer
-  // necessary
-  glDeleteShader(vertex);
-  glDeleteShader(fragment);
+  // vertex = glCreateShader(GL_VERTEX_SHADER);
+  // // glShaderSource(shader, 1, (const GLchar **)&shaderSource, nullptr);
+  // glShaderSource(vertex, 1, (const GLchar **)&vertexSource, NULL);
+  // glCompileShader(vertex);
+  // // checkCompileErrors(vertex, "VERTEX");
+  // // fragment Shader
+  // fragment = glCreateShader(GL_FRAGMENT_SHADER);
+  // glShaderSource(fragment, 1, (const GLchar **)&fragmentSource, NULL);
+  // glCompileShader(fragment);
+  // // checkCompileErrors(fragment, "FRAGMENT");
+  // // shader Program
+  // shader = glCreateProgram();
+  // glAttachShader(shader, vertex);
+  // glAttachShader(shader, fragment);
+  // glLinkProgram(shader);
+  // // checkCompileErrors(ID, "PROGRAM");
+  // // delete the shaders as they're linked into our program now and no longer
+  // // necessary
+  // glDeleteShader(vertex);
+  // glDeleteShader(fragment);
+  CShader *shader = &g_pHyprOpenGL->m_sWindowShader;
+  if (!shader->program) {
+    shader->program = g_pHyprOpenGL->createProgram(
+        TEXVERTSRC, myTEXFRAGSRCRGBAPASSTHRU, true);
+    shader->proj = glGetUniformLocation(shader->program, "proj");
+    shader->tex = glGetUniformLocation(shader->program, "tex");
+    shader->texAttrib = glGetAttribLocation(shader->program, "texcoord");
+    shader->posAttrib = glGetAttribLocation(shader->program, "pos");
+    std::cout << "GEN SHADER " << std::endl;
+  }
 
   int wid = in->m_vSize.x;
   int he = in->m_vSize.y;
@@ -121,7 +170,8 @@ void CWindowTransformer::preWindowRender(SRenderData *pRenderData,
   // glViewport(0, 0, wid, he);
 
   // cover entire screen
-  float quadVertices[] = {// vertex attributes for a quad that fills the entire
+  float quadVertices[] = {// vertex attributes for a quad that fills the
+                          // entire
                           // screen in Normalized Device Coordinates.
                           // positions   // texCoords
                           -1.0f, 1.0f, 0.0f, 1.0f,  -1.0f, -1.0f,
@@ -141,11 +191,40 @@ void CWindowTransformer::preWindowRender(SRenderData *pRenderData,
   glEnableVertexAttribArray(1);
   glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float),
                         (void *)(2 * sizeof(float)));
+  // fb.bind();
+
+  // GLint posAttrib = -1;
+  // GLint texAttrib = -1;
+  // glVertexAttribPointer(shader.posAttrib, 2, GL_FLOAT, GL_FALSE, 0,
+  // fullVerts); glVertexAttribPointer(shader.texAttrib, 2, GL_FLOAT, GL_FALSE,
+  // 0, fullVerts);
+
+  // glEnableVertexAttribArray(shader.posAttrib);
+  // glEnableVertexAttribArray(shader.texAttrib);
+
+  // CBox newBox = *pBox;
+  // m_RenderData.renderModif.applyToBox(newBox);
+
+  // static auto PDIMINACTIVE =
+  //     CConfigValue<Hyprlang::INT>("decoration:dim_inactive");
+  // static auto PDT = CConfigValue<Hyprlang::INT>("debug:damage_tracking");
+
+  // // get transform
+  // const auto TRANSFORM = wlr_output_transform_invert(
+  //     !m_bEndFrame ? WL_OUTPUT_TRANSFORM_NORMAL
+  //                  : m_RenderData.pMonitor->transform);
+  // float matrix[9];
+  // wlr_matrix_project_box(matrix, newBox.pWlr(), TRANSFORM, newBox.rot,
+  //                        m_RenderData.pMonitor->projMatrix.data());
+
+  // float glMatrix[9];
+  // wlr_matrix_multiply(glMatrix, m_RenderData.projection, matrix);
 
   // apply shader
-  glUseProgram(shader);
-  glUniform1i(glGetUniformLocation(shader, "texture0"), 0);
-  fb.bind();
+  glUseProgram(shader->program);
+  // glUniformMatrix3fv(shader->proj, 1, GL_TRUE, glMatrix);
+  glUniform1i(shader->tex, 0);
+  // glUniform1i(glGetUniformLocation(shader, "texture0"), 0);
 
   // GLuint FFrameBuffer;
   // setup FBO
@@ -153,7 +232,10 @@ void CWindowTransformer::preWindowRender(SRenderData *pRenderData,
   // glBindFramebuffer(GL_FRAMEBUFFER, FFrameBuffer);
   glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,
                          tex.m_iTexID, 0);
-  glBindTexture(GL_TEXTURE_2D, tex.m_iTexID);
+
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(tex.m_iTarget, tex.m_iTexID);
+  // glBindTexture(GL_TEXTURE_2D, tex.m_iTexID);
   glDrawArrays(GL_TRIANGLES, 0, 6);
   // // glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
@@ -162,8 +244,9 @@ void CWindowTransformer::preWindowRender(SRenderData *pRenderData,
       new GLubyte[wid * he * 4]; // Assuming RGBA for 4 bytes per pixel
   glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
   int a = rand();
-  std::string str = "outout_" + std::to_string(a);
+  std::string str = "/home/air/tmp/outout_" + std::to_string(a);
   stbi_write_png(str.c_str(), wid, he, 4, pixels, wid * 4);
+  fb.release();
 
   // TODO: do I need to copy the target back into tex.m_iTexID? (is already
   // there right?)
