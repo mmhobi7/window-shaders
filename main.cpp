9aac5ae6dc8c56d6b766e964e914a74f24f03ab5
M <woohoomoo2u@gmail.com>
2024-04-08 01:27:54 -0400

    clean up commented code

--- main.cpp
+++ main.cpp
@@ -57,17 +57,20 @@ color.r = 0.5;
 gl_FragColor = vec4(color, 1.0);
 })#";
 
+void writeToFile(int wid, int he){
+  GLubyte *pixels = new GLubyte[wid * he * 4];
+  glReadPixels(0, 0, wid, he, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+  int a = rand();
+  std::string str = "/home/aaahh/tmp/outout_" + std::to_string(a);
+  stbi_write_png(str.c_str(), wid, he, 4, pixels, wid * 4);
+}
+
 void CWindowTransformer::preWindowRender(SRenderData *pRenderData)
 {
-
-  // oldPos = {(double)pRenderData->x, (double)pRenderData->y};
-  // fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
-
   const auto TEXTURE = wlr_surface_get_texture(pRenderData->surface);
   const CTexture &tex = TEXTURE;
   int wid = tex.m_vSize.x;
   int he = tex.m_vSize.y;
-  GLubyte *pixels = new GLubyte[wid * he * 4];
 
   // if (!fb.isAllocated() || fb.m_vSize.x != pRenderData->w ||
   //     fb.m_vSize.y != pRenderData->h)
@@ -76,7 +79,7 @@ void CWindowTransformer::preWindowRender(SRenderData *pRenderData)
   //   fb.alloc(pRenderData->w, pRenderData->h);
   // }
   // fb.bind();
-  g_pHyprOpenGL->clear(CColor(0, 0, 0, 0));
+  // g_pHyprOpenGL->clear(CColor(0, 0, 0, 0));
 
   CShader *shader = &g_pHyprOpenGL->m_sWindowShader;
   if (!shader->program)
@@ -89,18 +92,7 @@ void CWindowTransformer::preWindowRender(SRenderData *pRenderData)
     shader->posAttrib = glGetAttribLocation(shader->program, "pos");
     std::cout << "GEN SHADER " << std::endl;
   }
-
-  // int wid = in->m_vSize.x;
-  // int he = in->m_vSize.y;
-  // std::cout << pRenderData->w << wid << std::endl;
-  // std::cout << pRenderData->h << he << std::endl;
-  // int wid = (int)std::round(pRenderData->pWindow->m_vReportedSize.x);
-  // int he = (int)std::round(pRenderData->pWindow->m_vReportedSize.x);
-  GLint m_viewport[4];
-  glGetIntegerv(GL_VIEWPORT, m_viewport);
-  std::cout << "YER1 " << m_viewport[0] << " " << m_viewport[1] << " " << m_viewport[2] << " " << m_viewport[3] << std::endl;
-  std::cout << "YER2 " << wid << " " << he << " " << std::endl;
-  // glViewport(0, 0, width, height);
+  
   glViewport(0, 0, wid, he);
 
   // unsigned int quadVAO, quadVBO;
@@ -138,57 +130,6 @@ void CWindowTransformer::preWindowRender(SRenderData *pRenderData)
   glEnableVertexAttribArray(shader->posAttrib);
   glEnableVertexAttribArray(shader->texAttrib);
 
-  // glEnableVertexAttribArray(0);
-  // glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void
-  // *)0);
-  // glEnableVertexAttribArray(1); glVertexAttribPointer(1, 2, GL_FLOAT,
-  // GL_FALSE, 4 * sizeof(float),
-  //                       (void *)(2 * sizeof(float)));
-  // fb.bind();
-
-  // GLint posAttrib = -1;
-  // GLint texAttrib = -1;
-  // glVertexAttribPointer(shader.posAttrib, 2, GL_FLOAT, GL_FALSE, 0,
-  // fullVerts); glVertexAttribPointer(shader.texAttrib, 2, GL_FLOAT, GL_FALSE,
-  // 0, fullVerts);
-
-  // glEnableVertexAttribArray(shader.posAttrib);
-  // glEnableVertexAttribArray(shader.texAttrib);
-
-  // CBox newBox = *pBox;
-  // m_RenderData.renderModif.applyToBox(newBox);
-
-  // static auto PDIMINACTIVE =
-  //     CConfigValue<Hyprlang::INT>("decoration:dim_inactive");
-  // static auto PDT = CConfigValue<Hyprlang::INT>("debug:damage_tracking");
-
-  // // get transform
-  // const auto TRANSFORM = wlr_output_transform_invert(
-  //     !m_bEndFrame ? WL_OUTPUT_TRANSFORM_NORMAL
-  //                  : m_RenderData.pMonitor->transform);
-  // float matrix[9];
-  // wlr_matrix_project_box(matrix, newBox.pWlr(), TRANSFORM, newBox.rot,
-  //                        m_RenderData.pMonitor->projMatrix.data());
-
-  // float glMatrix[9];
-  // wlr_matrix_multiply(glMatrix, m_RenderData.projection, matrix);
-
-  // apply shader
-  // glUseProgram(shader->program);
-  // glUniformMatrix3fv(shader->proj, 1, GL_TRUE, glMatrix);
-  // glUniform1i(shader->tex, 0);
-  // glUniform1i(glGetUniformLocation(shader, "texture0"), 0);
-
-  // GLuint FFrameBuffer;
-  // setup FBO
-  // glGenFramebuffers(1, &FFrameBuffer);
-  // glBindFramebuffer(GL_FRAMEBUFFER, FFrameBuffer);
-  // glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,
-  //  tex.m_iTexID, 0);
-
-  // glActiveTexture(GL_TEXTURE0);
-  // glBindTexture(GL_TEXTURE_2D, tex.m_iTexID);
-
   GLuint frameBuffer;
   glGenFramebuffers(1, &frameBuffer);
   glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
@@ -207,11 +148,6 @@ void CWindowTransformer::preWindowRender(SRenderData *pRenderData)
 
   glFramebufferTexture2D(
       GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texColorBuffer, 0);
-
-  // glActiveTexture(GL_TEXTURE0);
-  // glBindTexture(GL_TEXTURE_2D, tex.m_iTexID);
-
-  // glUseProgram(shader->program);
 
   GLuint frameBuffer2;
   glGenFramebuffers(1, &frameBuffer2);
@@ -250,11 +186,6 @@ void CWindowTransformer::preWindowRender(SRenderData *pRenderData)
   glViewport(0, 0, wid, he);
   glDrawArrays(GL_TRIANGLES, 0, 6);
 
-  // glReadPixels(0, 0, wid, he, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
-  // int a = rand();
-  // std::string str = "/home/aaahh/tmp/outout_" + std::to_string(a);
-  // stbi_write_png(str.c_str(), wid, he, 4, pixels, wid * 4);
-
   glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer2);
   glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT);
@@ -271,55 +202,6 @@ void CWindowTransformer::preWindowRender(SRenderData *pRenderData)
     std::cout << "looks good2" << std::endl;
   }
   glDrawArrays(GL_TRIANGLES, 0, 6);
-
-  glReadPixels(0, 0, wid, he, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
-  int a = rand();
-  std::string str = "/home/aaahh/tmp/outout_" + std::to_string(a);
-  stbi_write_png(str.c_str(), wid, he, 4, pixels, wid * 4);
-
-  // glBindTexture(GL_TEXTURE_2D, tex.m_iTexID);
-
-  // g_pHyprOpenGL->m_RenderData.clipBox = rg.getExtents();
-  // CRegion rg = pWindow->getFullWindowBoundingBox()
-  //                  .translate(-pMonitor->vecPosition +
-  //                             PWORKSPACE->m_vRenderOffset.value())
-  //                  .scale(pMonitor->scale);
-  // g_pHyprOpenGL->m_RenderData.clipBox = rg.getExtents();
-  // g_pHyprOpenGL->m_RenderData.clipBox
-  // CRegion rg = pRenderData->pWindow->getFullWindowBoundingBox();
-  // //                  //  .translate(-pRenderData->pMonitor->vecPosition)
-  // //                  .scale(pRenderData->pMonitor->scale);
-  // g_pHyprOpenGL->m_RenderData.clipBox = rg.getExtents();
-
-  // CRegion damageClip{0, 0, g_pHyprOpenGL->m_RenderData.clipBox.width,
-  //                    g_pHyprOpenGL->m_RenderData.clipBox.height};
-  // // damageClip.intersect(g_pHyprOpenGL->m_RenderData.damage);
-
-  // if (!damageClip.empty()) {
-  //   for (auto &RECT : damageClip.getRects()) {
-  //     g_pHyprOpenGL->scissor(&RECT);
-  //     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-  //   }
-  // }
-
-  // glDrawArrays(GL_TRIANGLES, 0, 8); // 54
-  // // glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-
-  // save texture to file (for debugging)
-
-  // GLuint fbo;
-  // glGenFramebuffers(1, &fbo);
-  // glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-  // glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tex.m_iTexID, 0);
-  // glBindTexture(GL_TEXTURE_2D, tex.m_iTexID);
-  // glDrawArrays(GL_TRIANGLES, 0, 8);
-
-  // glBindFramebuffer(GL_FRAMEBUFFER, 0);
-  // glDeleteFramebuffers(1, &fbo);
-
-  // glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
-
-  // // fb.release();
 }
 
 CFramebuffer *CWindowTransformer::transform(CFramebuffer *in) { return in; }
@@ -348,7 +230,7 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle)
   {
     HyprlandAPI::addNotification(
         PHANDLE,
-        "[hyprbars] Failure in initialization: Version mismatch (headers ver "
+        "[window-shaders] Failure in initialization: Version mismatch (headers ver "
         "is not equal to running hyprland ver)",
         CColor{1.0, 0.2, 0.2, 1.0}, 5000);
     throw std::runtime_error("[hb] Version mismatch");
