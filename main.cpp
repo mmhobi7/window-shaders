a7d2fe0444095f9d75fcd33006c427894d8800ac
mmhobi7 <woohoomoo2u@gmail.com>
2024-04-16 00:37:40 -0400

    format

--- main.cpp
+++ main.cpp
@@ -45,37 +45,33 @@ void main() {
     gl_FragColor = color;
 })#";
 
-std::string loadShader(std::string path)
-{
+std::string loadShader(std::string path) {
   if (path == "" || path == STRVAL_EMPTY)
     return "";
 
   std::ifstream infile(absolutePath(path, g_pConfigManager->getConfigDir()));
 
-  if (!infile.good())
-  {
-    g_pConfigManager->addParseError("[window-shaders]: Screen shader parser: Screen shader path not found");
+  if (!infile.good()) {
+    g_pConfigManager->addParseError(
+        "[window-shaders]: Screen shader parser: Screen shader path not found");
     return "";
   }
 
-  std::string shader((std::istreambuf_iterator<char>(infile)), (std::istreambuf_iterator<char>()));
+  std::string shader((std::istreambuf_iterator<char>(infile)),
+                     (std::istreambuf_iterator<char>()));
   return shader;
 }
 
-class CWindowTransformer : public IWindowTransformer
-{
+class CWindowTransformer : public IWindowTransformer {
 public:
-  CWindowTransformer(std::string fragmentShader, std::string vertexShader)
-  {
+  CWindowTransformer(std::string fragmentShader, std::string vertexShader) {
     std::string fragShader = loadShader(fragmentShader);
-    if (!fragShader.empty())
-    {
+    if (!fragShader.empty()) {
       this->fragmentShader = fragShader;
     }
 
     std::string vertShader = loadShader(vertexShader);
-    if (!vertShader.empty())
-    {
+    if (!vertShader.empty()) {
       vertShader = vertShader;
     }
   }
@@ -92,23 +88,16 @@ private:
 
 std::vector<CWindowTransformer *> ptrs;
 
-GLfloat identityMatrix[9] = {
-    1.0f, 0.0f, 0.0f,
-    0.0f, 1.0f, 0.0f,
-    0.0f, 0.0f, 1.0f};
+GLfloat identityMatrix[9] = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+                             0.0f, 0.0f, 0.0f, 1.0f};
 
 GLfloat quadVert[] = {
-    1.0f, -1.0f, 1.0f, 0.0f,
-    1.0f, 1.0f, 1.0f, 1.0f,
-    -1.0f, 1.0f, 0.0f, 1.0f,
+    1.0f, -1.0f, 1.0f, 0.0f, 1.0f,  1.0f, 1.0f, 1.0f, -1.0f, 1.0f,  0.0f, 1.0f,
 
-    1.0f, -1.0f, 1.0f, 0.0f,
-    -1.0f, 1.0f, 0.0f, 1.0f,
-    -1.0f, -1.0f, 0.0f, 0.0f};
+    1.0f, -1.0f, 1.0f, 0.0f, -1.0f, 1.0f, 0.0f, 1.0f, -1.0f, -1.0f, 0.0f, 0.0f};
 
 // Derived class inheriting from Base
-class CShaderEXT : public CShader
-{
+class CShaderEXT : public CShader {
 public:
   GLint vbo = -1;
   GLuint quadVAO = -1;
@@ -117,8 +106,7 @@ public:
 
 CShaderEXT shader;
 
-void writeToFile(int width, int height)
-{
+void writeToFile(int width, int height) {
   GLubyte *pixels = new GLubyte[width * height * 4];
   glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
   int a = rand();
@@ -132,13 +120,11 @@ void writeToFile(int width, int height)
 //   return std::string(strPtr);
 // }
 
-std::string convertToString(std::string strPtrPtr)
-{
+std::string convertToString(std::string strPtrPtr) {
   // // if ((std::string)strPtrPtr.length())
   std::cerr << "Error: eee is null." << std::endl;
-  HyprlandAPI::addNotification(
-      PHANDLE, "[window-shaders]" + strPtrPtr,
-      CColor{0.2, 1.0, 0.2, 1.0}, 5000);
+  HyprlandAPI::addNotification(PHANDLE, "[window-shaders]" + strPtrPtr,
+                               CColor{0.2, 1.0, 0.2, 1.0}, 5000);
   // if (strPtrPtr == nullptr)
   // {
   //   std::cerr << "Error: strPtrPtr is null." << std::endl;
@@ -150,23 +136,21 @@ std::string convertToString(std::string strPtrPtr)
   return strPtrPtr;
 }
 
-void CWindowTransformer::preWindowRender(SRenderData *pRenderData)
-{
+void CWindowTransformer::preWindowRender(SRenderData *pRenderData) {
   const auto TEXTURE = wlr_surface_get_texture(pRenderData->surface);
   const CTexture &windowTexture = TEXTURE;
 
   width = windowTexture.m_vSize.x;
   height = windowTexture.m_vSize.y;
 
-  if (!fb.isAllocated() || fb.m_vSize != windowTexture.m_vSize)
-  {
+  if (!fb.isAllocated() || fb.m_vSize != windowTexture.m_vSize) {
     fb.release();
     fb.alloc(width, height);
   }
 
-  CShader *passThroughShader = &g_pHyprOpenGL->m_RenderData.pCurrentMonData->m_shPASSTHRURGBA;
-  if (!shader.program)
-  {
+  CShader *passThroughShader =
+      &g_pHyprOpenGL->m_RenderData.pCurrentMonData->m_shPASSTHRURGBA;
+  if (!shader.program) {
     shader.program = g_pHyprOpenGL->createProgram(
         myTEXVERTSRC, myTEXFRAGSRCRGBAPASSTHRU, false);
     shader.proj = glGetUniformLocation(shader.program, "proj");
@@ -179,8 +163,10 @@ void CWindowTransformer::preWindowRender(SRenderData *pRenderData)
     glBindVertexArray(shader.quadVAO);
     glBindBuffer(GL_ARRAY_BUFFER, shader.quadVBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(quadVert), &quadVert, GL_STATIC_DRAW);
-    glVertexAttribPointer(shader.posAttrib, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
-    glVertexAttribPointer(shader.texAttrib, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (void *)(2 * sizeof(GLfloat)));
+    glVertexAttribPointer(shader.posAttrib, 2, GL_FLOAT, GL_FALSE,
+                          4 * sizeof(GLfloat), 0);
+    glVertexAttribPointer(shader.texAttrib, 2, GL_FLOAT, GL_FALSE,
+                          4 * sizeof(GLfloat), (void *)(2 * sizeof(GLfloat)));
     glEnableVertexAttribArray(shader.posAttrib);
     glEnableVertexAttribArray(shader.texAttrib);
 
@@ -193,7 +179,8 @@ void CWindowTransformer::preWindowRender(SRenderData *pRenderData)
   glBindFramebuffer(GL_FRAMEBUFFER, fb.m_iFb);
 
   // ping: render with user's shader
-  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, fb.m_cTex.m_iTexID, 0);
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,
+                         fb.m_cTex.m_iTexID, 0);
   glClear(GL_COLOR_BUFFER_BIT);
   glBindTexture(GL_TEXTURE_2D, windowTexture.m_iTexID);
 
@@ -205,7 +192,8 @@ void CWindowTransformer::preWindowRender(SRenderData *pRenderData)
   // writeToFile(width, height);
 
   // pong: write back to window texture
-  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, windowTexture.m_iTexID, 0);
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,
+                         windowTexture.m_iTexID, 0);
   glClear(GL_COLOR_BUFFER_BIT);
   glBindTexture(GL_TEXTURE_2D, fb.m_cTex.m_iTexID);
 
@@ -220,66 +208,75 @@ void CWindowTransformer::preWindowRender(SRenderData *pRenderData)
 
 CFramebuffer *CWindowTransformer::transform(CFramebuffer *in) { return in; }
 
-static void onNewWindow(void *self, std::any data)
-{
+static void onNewWindow(void *self, std::any data) {
   // data is guaranteed
   auto *const PWINDOW = std::any_cast<CWindow *>(data);
-  static auto *const PCLASS = (Hyprlang::STRING const *)HyprlandAPI::getConfigValue(PHANDLE, "plugin:window-shaders:class")->getDataStaticPtr();
-  if (((std::string)*PCLASS).length() == 0 || // select all windows if not specified
-      (g_pCompositor->m_pLastWindow && g_pCompositor->m_pLastWindow->m_szInitialClass == *PCLASS && g_pCompositor->m_pLastMonitor))
-  {
-    static auto *const PFRAGMENTSHADER = (Hyprlang::STRING const *)HyprlandAPI::getConfigValue(PHANDLE, "plugin:fragment-shader:class")->getDataStaticPtr();
-    static auto *const PVERTEXSHADER = (Hyprlang::STRING const *)HyprlandAPI::getConfigValue(PHANDLE, "plugin:vertex-shader:class")->getDataStaticPtr();
+  static auto *const PCLASS =
+      (Hyprlang::STRING const *)HyprlandAPI::getConfigValue(
+          PHANDLE, "plugin:window-shaders:class")
+          ->getDataStaticPtr();
+  if (((std::string)*PCLASS).length() ==
+          0 || // select all windows if not specified
+      (g_pCompositor->m_pLastWindow &&
+       g_pCompositor->m_pLastWindow->m_szInitialClass == *PCLASS &&
+       g_pCompositor->m_pLastMonitor)) {
+    static auto *const PFRAGMENTSHADER =
+        (Hyprlang::STRING const *)HyprlandAPI::getConfigValue(
+            PHANDLE, "plugin:window-shaders:fragment-shader")
+            ->getDataStaticPtr();
+    static auto *const PVERTEXSHADER =
+        (Hyprlang::STRING const *)HyprlandAPI::getConfigValue(
+            PHANDLE, "plugin:vertex-shader:class")
+            ->getDataStaticPtr();
 
     // TODO pass PFRAGMENTSHADER and PVERTEXSHADER
-    PWINDOW->m_vTransformers.push_back(std::make_unique<CWindowTransformer>("", ""));
+    PWINDOW->m_vTransformers.push_back(
+        std::make_unique<CWindowTransformer>("", ""));
   }
 }
 
-APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle)
-{
+APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
   PHANDLE = handle;
 
   const std::string HASH = __hyprland_api_get_hash();
 
-  if (HASH != GIT_COMMIT_HASH)
-  {
-    HyprlandAPI::addNotification(
-        PHANDLE,
-        "[window-shaders] Failure in initialization: Version mismatch (headers ver "
-        "is not equal to running hyprland ver)",
-        CColor{1.0, 0.2, 0.2, 1.0}, 5000);
+  if (HASH != GIT_COMMIT_HASH) {
+    HyprlandAPI::addNotification(PHANDLE,
+                                 "[window-shaders] Failure in initialization: "
+                                 "Version mismatch (headers ver "
+                                 "is not equal to running hyprland ver)",
+                                 CColor{1.0, 0.2, 0.2, 1.0}, 5000);
     throw std::runtime_error("[window-shaders] Version mismatch");
   }
 
-  HyprlandAPI::addConfigValue(PHANDLE, "plugin:window-shaders:class", Hyprlang::STRING{""});
-  HyprlandAPI::addConfigValue(PHANDLE, "plugin:window-shaders:fragment-shader", Hyprlang::STRING{"br"});
-  HyprlandAPI::addConfigValue(PHANDLE, "plugin:window-shaders:vertex-shader", Hyprlang::STRING{"ee"});
+  HyprlandAPI::addConfigValue(PHANDLE, "plugin:window-shaders:class",
+                              Hyprlang::STRING{""});
+  HyprlandAPI::addConfigValue(PHANDLE, "plugin:window-shaders:fragment-shader",
+                              Hyprlang::STRING{"br"});
+  HyprlandAPI::addConfigValue(PHANDLE, "plugin:window-shaders:vertex-shader",
+                              Hyprlang::STRING{"ee"});
 
   HyprlandAPI::registerCallbackDynamic(
       PHANDLE, "openWindow",
-      [&](void *self, SCallbackInfo &info, std::any data)
-      {
+      [&](void *self, SCallbackInfo &info, std::any data) {
         onNewWindow(self, data);
       });
 
-  HyprlandAPI::addNotification(
-      PHANDLE, "[window-shaders] Initialized successfully!",
-      CColor{0.2, 1.0, 0.2, 1.0}, 5000);
+  HyprlandAPI::addNotification(PHANDLE,
+                               "[window-shaders] Initialized successfully!",
+                               CColor{0.2, 1.0, 0.2, 1.0}, 5000);
 
   return {"window-shaders", "Apply shaders on each window", "M", "1.0"};
 }
 
-APICALL EXPORT void PLUGIN_EXIT()
-{
-  for (auto &w : g_pCompositor->m_vWindows)
-  {
+APICALL EXPORT void PLUGIN_EXIT() {
+  for (auto &w : g_pCompositor->m_vWindows) {
     std::erase_if(
         w->m_vTransformers,
-        [&](const std::unique_ptr<IWindowTransformer> &other)
-        {
-          return std::find_if(ptrs.begin(), ptrs.end(), [&](const auto &p)
-                              { return static_cast<IWindowTransformer *>(p) == other.get(); }) != ptrs.end();
+        [&](const std::unique_ptr<IWindowTransformer> &other) {
+          return std::find_if(ptrs.begin(), ptrs.end(), [&](const auto &p) {
+                   return static_cast<IWindowTransformer *>(p) == other.get();
+                 }) != ptrs.end();
         });
   }
 }
